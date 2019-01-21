11cceecd7f5020a5ed2bb1be396c959dfd57df41
da Vinci <brucewilson1103@gmail.com>
2019-01-21 12:27:24 -0500

    javascript linking the add event listener keypress to the sounds

---
+++ index.js
@@ -0,0 +1,51 @@
+alert("test")
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+
+  // listening for button press
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+      var buttonInnerHTML = this.innerHTML;
+      drumbeat(buttonInnerHTML)
+
+  });
+}
+// listening for key press
+document.addEventListener("keypress", function (event) {
+  drumbeat(event.key);
+});
+
+function drumbeat(key) {
+  switch (key) {
+      case "w":
+
+          var tom1 = new Audio('/sounds/tom-1.mp3');
+          tom1.play();
+          break;
+      case "a":
+          var tom2 = new Audio('/sounds/tom-2.mp3');
+          tom2.play();
+          break;
+      case "s":
+          var tom3 = new Audio('/sounds/tom-3.mp3');
+          tom3.play();
+          break;
+      case "d":
+          var tom4 = new Audio('/sounds/tom-4.mp3');
+          tom4.play();
+          break;
+      case "j":
+          var snaredrum = new Audio('/sounds/snare.mp3');
+          snaredrum.play();
+          break;
+      case "k":
+          var crash1 = new Audio('/sounds/crash.mp3');
+          crash1.play();
+          break;
+      case "l":
+          var kick = new Audio('/sounds/kick-bass.mp3');
+          kick.play();
+          break;
+      default:
+          console.log(buttonInnerHTML);
+  }
+}
